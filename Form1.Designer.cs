9808622c659396613ba9634de787ab4fc98b2213
DropTheBeat04 <gabrielbotha5@gmail.com>
2024-11-04 19:37:08 +0200

    Renaming a tab

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -94,6 +94,7 @@
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage1.Size = new System.Drawing.Size(964, 609);
             this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "View All Students";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // button8
