6679324a90d9657187c50224b0374002f9f198b6
rourke Veller <rourke@nitida.co.za>
2024-11-05 12:28:25 +0200

    adding text file and getting the data to display.

---
+++ Business Layer/Data Manipulation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PRG282_Project.Data_Access_Layer;
+
+namespace PRG282_Project.Business_Layer
+{
+    internal class Data_Manipulation
+    {
+        public DataTable display()
+        {
+            DataHandler dataHandler = new DataHandler();
+
+            DataTable studentData = new DataTable();
+
+            studentData.Columns.Add("ID", typeof(string));
+            studentData.Columns.Add("Name", typeof(string));
+            studentData.Columns.Add("Date", typeof(string));
+            studentData.Columns.Add("Course", typeof(string));
+
+            List<string[]> displayList = dataHandler.fillList();
+
+            foreach (var item in displayList)
+            {
+                studentData.Rows.Add(item);
+            }
+
+            return studentData;
+        }
+    }
+}
---
+++ Data Access Layer/DataHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PRG282_Project.Data_Access_Layer
+{
+    internal class DataHandler
+    {
+        public static string filepath = @"C:\Users\Rourke\Desktop\Belgium Campus Work\PRG282\PRG282 Project\Student_Info.txt";
+
+        private string[] students = File.ReadAllLines(filepath);
+
+        public List<string[]> manipList = new List<string[]>();
+
+        public List<string[]> fillList()
+        {
+            manipList.Clear();
+            foreach (var item in students)
+            {
+                manipList.Add(item.Split(','));
+            }
+            return manipList;
+        }
+    }
+}
