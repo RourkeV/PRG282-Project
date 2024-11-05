997f1f7a9ba66a568a9ace9e739201f354a8d91c
DropTheBeat04 <gabrielbotha5@gmail.com>
2024-11-05 21:39:50 +0200

    Switched from tab control to using user control. Added UpdateUser, AddUser,Home and ViewAllStudents form and created the navbar

--- Data Access Layer/DataHandler.cs
+++ Data Access Layer/DataHandler.cs
@@ -9,7 +9,8 @@ namespace PRG282_Project.Data_Access_Layer
 {
     internal class DataHandler
     {
-        public static string filepath = @"C:\Users\Rourke\Desktop\Belgium Campus Work\PRG282\PRG282 Project\Student_Info.txt";
+        //public static string filepath = @"C:\Users\Rourke\Desktop\Belgium Campus Work\PRG282\PRG282 Project\Student_Info.txt";
+        public static string filepath = @"C:\Users\gabri\OneDrive - belgiumcampus.ac.za\Desktop\PRG282 Project Pt2\Student_Info.txt";
 
         private string[] students = File.ReadAllLines(filepath);
 
--- Presentation Layer/HomePage.cs
+++ Presentation Layer/HomePage.cs
@@ -9,6 +9,9 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using PRG282_Project.Data_Access_Layer;
 using PRG282_Project.Business_Layer;
+using System.Web.UI.Design.WebControls;
+using System.Xml.Linq;
+using PRG282_Project.UserControls;
 
 namespace PRG282_Project
 {
@@ -18,19 +21,60 @@ namespace PRG282_Project
         public HomePage()
         {
             InitializeComponent();
+            Home uc = new Home();
+            addUserControl(uc);
         }
 
         private void HomePage_Load(object sender, EventArgs e)
         {
-            Data_Manipulation dm = new Data_Manipulation();
-
-            viewStudents.DataSource = dm.display();
+            
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
             AddStudentForm add = new AddStudentForm();
             add.Show();
+        }
+      
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void addUserControl(UserControl userControl)
+        {
+            userControl.Dock = DockStyle.Fill;
+            panelContainer.Controls.Clear();
+            panelContainer.Controls.Add(userControl);
+            userControl.BringToFront();
+        }
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            Home uc = new Home();
+            addUserControl(uc);
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            AddStudent uc = new AddStudent();
+            addUserControl(uc);
+        }
+
+        private void btnViewAll_Click(object sender, EventArgs e)
+        {
+            ViewAllStudents uc = new ViewAllStudents();
+            addUserControl(uc);
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            UpdateStudent uc = new UpdateStudent();
+            addUserControl(uc);
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
         }
     }
 }
